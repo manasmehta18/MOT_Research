5dad3799ca28584921dcfd70d0e37b8bae392580
Manas Mehta <manasmehta.18@gmail.com>
2021-07-15 01:56:37 -0400

    minor format fixes

--- detect.py
+++ detect.py
@@ -12,17 +12,28 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 cap = cv2.VideoCapture('vid.mp4') 
 
 imageNum = 0
-path = 'C:/Users/manas/Desktop/sort/detect'
+path = 'C:/Users/manas/Desktop/sort/yeet/img1'
 
-f = open("C:/Users/manas/Desktop/sort/detections.txt", "a")
+f = open("C:/Users/manas/Desktop/sort/yeet/det/det.txt", "a")
 
 while True:
 	imageNum += 1
 
 	_, image = cap.read()  
 
-	image = imutils.resize(image, width=min(800, image.shape[1]))
+	image = imutils.resize(image, width=min(640, image.shape[1]))
 	orig = image.copy()
+
+	if (imageNum < 10):
+		cv2.imwrite(os.path.join(path , '00000'+ str(imageNum) + '.jpg'), image)
+	elif (imageNum >= 10 and imageNum < 100):
+		cv2.imwrite(os.path.join(path , '0000'+ str(imageNum) + '.jpg'), image)
+	elif (imageNum >= 100 and imageNum < 1000):
+		cv2.imwrite(os.path.join(path , '000'+ str(imageNum) + '.jpg'), image)
+	elif (imageNum >= 1000 and imageNum < 10000):
+		cv2.imwrite(os.path.join(path , '00'+ str(imageNum) + '.jpg'), image)
+	else:
+		cv2.imwrite(os.path.join(path , '0'+ str(imageNum) + '.jpg'), image)
 
 	# detect people in the image
 	(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
@@ -46,17 +57,6 @@ while True:
 		print(str(imageNum) + ",-1" + ',' + str(xA) + "," + str(yA) + "," + str(xB) + "," + str(yB) + "," + str(format(weights[i,0], ".3f")) + ',' + "-1,-1,-1\n")
 		f.write(str(imageNum) + ",-1" + ',' + str(xA) + "," + str(yA) + "," + str(xB) + "," + str(yB) + "," + str(format(weights[i,0], ".3f")) + ',' + "-1,-1,-1\n")
 		i += 1
-
-	if (imageNum < 10):
-		cv2.imwrite(os.path.join(path , '00000'+ str(imageNum) + '.jpg'), image)
-	elif (imageNum >= 10 and imageNum < 100):
-		cv2.imwrite(os.path.join(path , '0000'+ str(imageNum) + '.jpg'), image)
-	elif (imageNum >= 100 and imageNum < 1000):
-		cv2.imwrite(os.path.join(path , '000'+ str(imageNum) + '.jpg'), image)
-	elif (imageNum >= 1000 and imageNum < 10000):
-		cv2.imwrite(os.path.join(path , '00'+ str(imageNum) + '.jpg'), image)
-	else:
-		cv2.imwrite(os.path.join(path , '0'+ str(imageNum) + '.jpg'), image)
 
 	# print some info on the bounding boxes
 	print("[INFO]: {} people detected in the frame".format(len(pick)))
